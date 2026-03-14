fe09a34b0afee5ea94f5acaa5ed2f8a47edaaf8f
MeowWow520 <Meow114514@outlook.com>
2026-03-14 18:21:45 +0800

    Fixed

--- src/SceneMain.cpp
+++ src/SceneMain.cpp
@@ -3,9 +3,8 @@
 
 
 
-SceneMain::SceneMain() {
+SceneMain::SceneMain() : gameInstance(&Game::getInstance()) {
     spdlog::info(u8"Entering SceneMain::SceneMain()");
-    gameInstance = &Game::getInstance();
 }
 
 SceneMain::~SceneMain() { 
@@ -19,32 +18,33 @@ void SceneMain::Initialize() {
     player.texture = IMG_LoadTexture(gameInstance->getRenderer(), "assets/images/Test_MyHander.jpg");
     if (player.texture == nullptr) {
         spdlog::error(u8"Failed to load player texture: {}", SDL_GetError());
-        player.width = 100;
-        player.height = 100;
     } else { 
-        spdlog::info(u8"Player texture loaded successfully");
+        spdlog::info(u8"Player.texture Loaded Successfully");
         spdlog::info(u8"Initializing SDL_QueryTexture");
         SDL_QueryTexture(player.texture, NULL, NULL, &player.width, &player.height);
     }
+
+
     // Setting initial player position and size
-    spdlog::info(u8"Setting initial player position and size");
-    spdlog::info(u8"Original player width: {}, height: {}", player.width, player.height);
     player.width /= 4;
     player.height /= 4;
     player.position.x = (float)(gameInstance->getWindowWidth() / 2) - (float)(player.width / 2);
     player.position.y = (float)(gameInstance->getWindowHeight() - player.height);
-
+    spdlog::info(u8"Setting initial player position and size");
+    spdlog::info(u8"Original player width: {}, height: {}", player.width, player.height);
 }
 
-void SceneMain::handleEvents(SDL_Event *event) { }
+void SceneMain::handleEvents(SDL_Event *event) { return; }
 
-void SceneMain::Update() { }
+void SceneMain::Update() { return; }
 
 void SceneMain::Render() { 
     SDL_Rect playerRect = { static_cast<int>(player.position.x), 
                             static_cast<int>(player.position.y), 
                             player.width, player.height };
-    SDL_RenderCopy(gameInstance->getRenderer(), player.texture, NULL, &playerRect);
+    if (SDL_RenderCopy(gameInstance->getRenderer(), player.texture, NULL, &playerRect) != 0) {
+        spdlog::error(u8"SDL_RenderCopy Failed");
+    }
 }
 
 void SceneMain::Clean() { 
--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,8 @@
 #include "Game.h"
 
 
-#ifdef _WIN32
-#include <Windows.h>
-#endif
-
-void setup_console() {
-#ifdef _WIN32
-    // 设置控制台输出代码页为 UTF-8
-    SetConsoleOutputCP(CP_UTF8);
-    // 可选：如果需要从控制台读取中文，也设置输入代码页
-    SetConsoleCP(CP_UTF8);
-    spdlog::info(u8"Console code page set to UTF-8");
-#endif
-    return;
-}
-
 
 int main(int argc, char** argv) {
-    setup_console();
     spdlog::info(u8"Entering main.cpp -> main function");
     spdlog::info(u8"There is branch main");
     
